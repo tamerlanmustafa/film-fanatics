a15ba687d86f04d1e66e9ced3c5820fc3075f7e1
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-12-22 20:34:34 -0500

    add movie model

---
+++ models/movies.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+
+
+const userReviewSchema = new mongoose.Schema({
+    reviews: {type: String, required: true},
+    likes: { type: Number, default: 0},
+    dislikes: { type: Number, default: 0 },
+    userName: {type: String}
+});
+
+const movieSchema = new mongoose.Schema({
+    movieName : { type: String},
+    releaseYear:{type: Number},
+    genre: { type: String },
+    director: {type: String},
+    stillInTheaters: {type: Boolean},
+    description: {type: String},
+    coverPhotoUrl: {type:String},
+    userReviews: [userReviewSchema]
+});
+
+
+
+
+const UserReview = mongoose.model("UserReview",userReviewSchema)
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+module.exports = {
+    UserReview,
+    Movie
+};
